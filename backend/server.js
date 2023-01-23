0d751ca7f209645e506defa1d73d40975e012379
tonyqiu123 <tonyqiu12345@gmail.com>
2023-01-23 05:18:42 -0500

    added cors

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,14 @@ const {errorHandler} = require('./middleware/errorMiddleware')
 const app = express()
 const mongoose = require('mongoose')
 const connectDB = require('./config/db')
+const cors = require('cors')
 
 connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+app.use(cors())
 
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/jobs', require('./routes/jobRoutes'))
