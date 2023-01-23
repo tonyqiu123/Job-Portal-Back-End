107207485574854d14023cfbf823b59260348377
tonyqiu123 <tonyqiu12345@gmail.com>
2023-01-23 05:56:20 -0500

    updated jobs model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,11 +5,19 @@ const moment = require('moment')
 const userSchema = new Schema({
     userName: { type: String, required: true },
     email: { type: String, required: true },
+    profileURL: { type: String, default: "https://cdn.discordapp.com/attachments/715319623637270638/1067034270369054822/2048px-Default_pfp.png" },
     password: { type: String, required: true },
-    location: { type: String },
-    date: { type: String,  default: () => moment().format("MMM DD YYYY") },
-    appliedJobs: { type: [String] },
-    shortlistedJos: {type: [String]}
+    location: { type: String, default: "" },
+    phone: { type: String, default: "" },
+    links: { type: [String], default: [] },
+    attachments: { type: [String], default: [] },
+    experiences: { type: [String], default: [] },
+    projects: { type: [String], default: [] },
+    education: { type: [String], default: [] },
+    information: { type: [String], default: [] },
+    dateCreated: { type: String,  default: () => moment().format("MMM DD YYYY") },
+    appliedJobs: { type: [String], default: [] },
+    shortlistedJobs: {type: [String] , default: []}
   });
 
 const User = mongoose.model('User', userSchema);
