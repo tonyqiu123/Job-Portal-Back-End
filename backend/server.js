5676568f24eb6014a7e50b22a27f6bccb38f5a3a
tonyqiu123 <tonyqiu12345@gmail.com>
2023-01-25 18:34:03 -0500

    test

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use(cors())
+app.use(cors()) 
 
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/jobs', require('./routes/jobRoutes'))
