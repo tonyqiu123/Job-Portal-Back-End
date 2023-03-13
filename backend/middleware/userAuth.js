495f93196acdb9dce5a9e20de335bb051fec17c8
Tony Qiu <tonyqiu12345@gmail.com>
2023-03-13 12:04:51 -0400

    test

--- backend/middleware/userAuth.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-function generateToekn(payload, secret) {
-    return jwt.sign(payload,secret)
-}
-
-module.exports = {jwt}
--- backend/routes/adminRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { adminLogin } = require('../controllers/adminController')
-
-router.post('/login', adminLogin)
-
-module.exports = router 
